95eceeaf04ced15a494549cdd444025695effcf7
shmooel28 <shmooel28@gmail.com>
2023-07-22 21:30:51 +0300

    saved model

--- SVM.py
+++ SVM.py
@@ -3,7 +3,12 @@ import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuracy_score, f1_score
+import joblib
+import matplotlib.pyplot as plt
 
+# Initialize empty lists to store accuracy during training
+train_acc_history = []
+val_acc_history = []
 # Define a dictionary to map age labels to integers
 age_label2int = {
     "Top soil": 0,
@@ -29,8 +34,8 @@ labels = labels.astype(np.float32)
 features = features.astype(np.float32)
 
 # Split the data into training, validation, and testing sets
-X_train_val, X_test, y_train_val, y_test = train_test_split(features, labels, test_size=0.1, random_state=42)
-X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.1, random_state=42)
+X_train_val, X_test, y_train_val, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.3, random_state=42)
 
 print("X_train shape:", X_train.shape)
 print("X_val shape:", X_val.shape)
@@ -40,7 +45,7 @@ print("y_val shape:", y_val.shape)
 print("y_test shape:", y_test.shape)
 
 # Initialize the SVM model with a linear kernel
-svm_model = SVC(kernel='rbf')
+svm_model = SVC(kernel='linear')
 
 # Train the SVM model on the training data
 svm_model.fit(X_train, y_train)
@@ -62,3 +67,6 @@ f1 = f1_score(y_test, y_pred, average='weighted')
 
 print("Test Accuracy: {:.2f}%".format(accuracy * 100))
 print("Test F1 Score: {:.2f}".format(f1))
+
+model_filename = "svm_model.sav"
+joblib.dump(svm_model, model_filename)
